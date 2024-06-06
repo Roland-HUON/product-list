b35ccfef542c612d7cb2668b3dc1992c1b4a97d5
Roland HUON <huonroland91@gmail.com>
2024-06-06 16:38:36 +0200

    chore: Add search functionality and checkbox for filtering products

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { Fragment, useState } from "react"
+import PropTypes from "prop-types";
+
 function App() {
-  return 
+
+  const [productName, setProductName] = useState("Search...")
+  const [checked, setChecked] = useState(false)
+
+  return <Fragment>
+    <input type="text" value={productName} onChange={e => setProductName(e.target.value)} />
+    <CheckedBox checked={checked} onCheck={setChecked}/>
+  </Fragment>
+}
+
+function CheckedBox({checked, onCheck}){
+  return <label>
+    <input 
+      type="checkbox"
+      onChange={(e)=> onCheck(e.target.checked)}
+      checked={checked}
+    />
+    Show only products in stock
+  </label>
+}
+
+CheckedBox.propTypes = {
+  checked: PropTypes.bool.isRequired,
+  onCheck: PropTypes.func.isRequired
 }
 
 export default App
